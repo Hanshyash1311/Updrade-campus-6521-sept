a0c28a8258e27ef9d0826cc6f2839ae565937ddf
Hansraj shandilya vangipurapu <147262230+Hanshyash1311@users.noreply.github.com>
2023-10-23 01:49:39 +0530

    Update Different.java

--- Different.java
+++ Different.java
@@ -1,15 +1,10 @@
 //Write a program using different data types//
 import java.util.Scanner;
-
-public class Different {
-    // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-    class GetInputFromUser{
+ class GetInputFromUser{
 
 
         public static void main(String[] args) {
-            // Press Alt+Enter with your caret at the highlighted text to see how
-            // IntelliJ IDEA suggests fixing it.
+            
             //Using Scanner for getting Input from User
             Scanner in = new Scanner(System.in);
             String s =in.nextLine();
@@ -20,16 +15,6 @@ public class Different {
             System.out.println("You entered float"+b);
 
 
-
-
-
-            // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-
-
         }
     }
-}
+
