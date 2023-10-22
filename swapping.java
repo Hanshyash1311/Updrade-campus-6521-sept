6e1865a76d086453bd28326b2977fd8b2ecd016a
hansraj <vshansraj13@gmail.com>
2023-10-23 00:02:46 +0530

    Swapping two numbers with the help of third Variable

---
+++ swapping.java
@@ -0,0 +1,34 @@
+public class swapping
+{
+    // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import com.sun.jdi.Value;
+
+import java.util.*;
+    class GFG{
+        static void swapValuesUsingThirdVariable(int a, int b) {
+            // Press Alt+Enter with your caret at the highlighted text to see how
+            // IntelliJ IDEA suggests fixing it.
+            int c = a;
+            a = b;
+            b = c;
+
+            System.out.println("Value of a is" + a + "Value of b is"+b);
+
+        }
+        public static void main(String[] args)
+        {
+            int a=10,b=20;
+            swapValuesUsingThirdVariable(a,b);
+
+
+            // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+
+        }
+    }
+
+}
