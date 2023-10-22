15cd2f521de6fda9e8abcb603f18f3e50455f349
Hansraj shandilya vangipurapu <147262230+Hanshyash1311@users.noreply.github.com>
2023-10-23 00:04:05 +0530

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -1,4 +1,4 @@
-Q-// Write a program in java using while loop//
+Q-// Write a program in java //
 public class Test1 {
     // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
