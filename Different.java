4b93fbc588378ee52a23b143db90c125573af4d1
Hansraj shandilya vangipurapu <147262230+Hanshyash1311@users.noreply.github.com>
2023-10-23 00:28:57 +0530

    Update Different.java

--- Different.java
+++ Different.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Different {
     // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-import java.util.Scanner;
     class GetInputFromUser{
 
 
