007557d998a0e28162d4d10f5b1aeab78353ec47
Hansraj shandilya vangipurapu <147262230+Hanshyash1311@users.noreply.github.com>
2023-10-23 01:52:07 +0530

    Update Reversepyramid.java

--- Reversepyramid.java
+++ Reversepyramid.java
@@ -1,11 +1,6 @@
 //Write a program to draw Reverse pyramid//
-public class Reversepyramid {
-    // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
     public class Main {
         public static void main(String[] args) {
-            // Press Alt+Enter with your caret at the highlighted text to see how
-            // IntelliJ IDEA suggests fixing it.
             int a=10;
             for (int i=0;i<=a-1;i++){
                 for (int j=0;j<=i;j++){
@@ -27,4 +22,4 @@ public class Reversepyramid {
 
         }
     }
-}
+
