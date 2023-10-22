3c948a8a42629636b8356a33c42e2f081d1a1252
Hansraj shandilya vangipurapu <147262230+Hanshyash1311@users.noreply.github.com>
2023-10-23 00:41:45 +0530

    Update triangle.java

--- triangle.java
+++ triangle.java
@@ -1,3 +1,4 @@
+//Write a program to draw triangle using for loop//
 public class triangle {
     // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
