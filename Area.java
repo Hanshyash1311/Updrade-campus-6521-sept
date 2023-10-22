3dcee9c6866b67a1d264790635f6169ad0a377d1
hansraj <vshansraj13@gmail.com>
2023-10-23 00:51:13 +0530

    Area of triangle

---
+++ Area.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Area {
+    import java.util.Scanner;
+
+    // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+    public class Main {
+        public static void main(String[] args) {
+            Scanner scan= new Scanner(System.in);
+            System.out.print("Enter the base:");
+            int base= scan.nextInt();
+            System.out.print("Enter the height:");
+            int height= scan.nextInt();
+            double area =(0.5)*(base*height);
+            System.out.println("The area of triangle is:"+area);
+
+
+
+
+        }
+    }
+}
