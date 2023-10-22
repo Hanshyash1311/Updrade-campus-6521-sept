a58f3c2058947900456fd229689f05a19283c5fd
Hansraj shandilya vangipurapu <147262230+Hanshyash1311@users.noreply.github.com>
2023-10-23 02:06:05 +0530

    Update swapping.java

--- swapping.java
+++ swapping.java
@@ -1,15 +1,9 @@
 //Write a program for Swapping two variables with help of third variable.//
-public class swapping
-{
-    // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 import com.sun.jdi.Value;
 
 import java.util.*;
     class GFG{
         static void swapValuesUsingThirdVariable(int a, int b) {
-            // Press Alt+Enter with your caret at the highlighted text to see how
-            // IntelliJ IDEA suggests fixing it.
             int c = a;
             a = b;
             b = c;
@@ -23,13 +17,9 @@ import java.util.*;
             swapValuesUsingThirdVariable(a,b);
 
 
-            // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
+        
 
         }
     }
 
-}
+
