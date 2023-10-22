1b87323b6b1039810dbfacd75af09ec3b465c4c0
Hansraj shandilya vangipurapu <147262230+Hanshyash1311@users.noreply.github.com>
2023-10-23 02:01:34 +0530

    Update forloop.java

--- forloop.java
+++ forloop.java
@@ -1,20 +1,10 @@
 //write a program using for loop//
-public class forloop {
-    // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
     public class Main {
         public static void main(String[] args) {
-            // Press Alt+Enter with your caret at the highlighted text to see how
-            // IntelliJ IDEA suggests fixing it.
             System.out.printf("welcome to project!");
-
-            // Press Shift+F10 or click the green arrow button in the gutter to run the code.
             for (int i = 2; i <= 8; i++) {
-
-                // Press Shift+F9 to start debugging your code. We have set one breakpoint
-                // for you, but you can always add more by pressing Ctrl+F8.
                 System.out.println("i = " + i);
             }
         }
     }
-}
+
