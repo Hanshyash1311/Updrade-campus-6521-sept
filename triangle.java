2a1ca2ab6d30f91e1a1187a0ec0887c9e046694d
Hansraj shandilya vangipurapu <147262230+Hanshyash1311@users.noreply.github.com>
2023-10-23 02:18:36 +0530

    Update triangle.java

--- triangle.java
+++ triangle.java
@@ -1,11 +1,6 @@
 //Write a program to draw triangle using for loop//
-public class triangle {
-    // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-
-        public static void main(String[] args) {
-            // Press Alt+Enter with your caret at the highlighted text to see how
-            // IntelliJ IDEA suggests fixing it.
+public class Main{           
+public static void main(String[] args) {
             //i is for rows and j is for columns
             int i, j, p = 8;
             for (i = 0; i < p; i++) {
@@ -18,11 +13,6 @@ public class triangle {
 
                 System.out.println();
 
-                // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-
-
-                // Press Shift+F9 to start debugging your code. We have set one breakpoint
-                // for you, but you can always add more by pressing Ctrl+F8.
             }
         }
     }
