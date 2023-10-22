a39c711d5cad17ad9d13d7ff34d702c7716db902
hansraj <vshansraj13@gmail.com>
2023-10-23 00:08:05 +0530

    Take different integer string and float

---
+++ Different.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Different {
+    // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+    class GetInputFromUser{
+
+
+        public static void main(String[] args) {
+            // Press Alt+Enter with your caret at the highlighted text to see how
+            // IntelliJ IDEA suggests fixing it.
+            //Using Scanner for getting Input from User
+            Scanner in = new Scanner(System.in);
+            String s =in.nextLine();
+            System.out.printf("You entered string"+s);
+            int a = in.nextInt();
+            System.out.println("You entered integer"+a);
+            float b= in.nextFloat();
+            System.out.println("You entered float"+b);
+
+
+
+
+
+            // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+
+
+        }
+    }
+}
