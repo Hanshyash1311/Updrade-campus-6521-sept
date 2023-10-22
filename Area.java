d8a25b0632fbe7128c5e932d092dc05bc4977d0a
Hansraj shandilya vangipurapu <147262230+Hanshyash1311@users.noreply.github.com>
2023-10-23 01:46:36 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -1,11 +1,6 @@
 // Write a program to find the area of triangle.//
 import java.util.Scanner;
-
-public class Area {
-    import java.util.Scanner;
-
-    // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+    
     public class Main {
         public static void main(String[] args) {
             Scanner scan= new Scanner(System.in);
@@ -21,4 +16,4 @@ public class Area {
 
         }
     }
-}
+
