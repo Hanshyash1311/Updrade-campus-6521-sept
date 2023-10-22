f079926f1757118e19608bfa994a002bf97a690c
hansraj <vshansraj13@gmail.com>
2023-10-23 01:41:48 +0530

    Using else if statement

---
+++ elseif.java
@@ -0,0 +1,28 @@
+public class elseif {
+    // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+    public class Main {
+        public static void main(String[] args) {
+            // Press Alt+Enter with your caret at the highlighted text to see how
+            // IntelliJ IDEA suggests fixing it.
+            int number=100;
+            if (number>100) {
+                System.out.println("The number is greater than 100");
+            } else if (number<100) {
+                System.out.println("the number is less than 100");
+
+            }
+            else {
+                System.out.println("The number is equal to 100");
+            }
+
+
+            // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+
+        }
+    }
+
+}
